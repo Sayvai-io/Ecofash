4be720b53b7d1a4ffa5ad8096c777fa19c4f76d5
sayvai <prakash@sayvai.io>
2024-09-30 19:31:03 +0530

    about image changed

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -44,9 +44,9 @@ const About = () => {
   return (
     <>
       {/* <!-- ===== About Start ===== --> */}
-      <section className="mt-20 overflow-hidden pb-20 lg:pb-25 xl:pb-30">
+      <section className="mt-20 overflow-hidden pb-20 lg:pb-25 xl:pb-30 ">
         <div className="mx-auto max-w-c-1235 px-4 md:px-8 xl:px-8">
-          <div className="flex flex-col items-center gap-8 md:flex-row lg:gap-32.5">
+          <div className="flex flex-col items-center gap-8 md:flex-row lg:gap-10">
             <motion.div
               variants={{
                 hidden: {
@@ -64,10 +64,10 @@ const About = () => {
               transition={{ duration: 0.5, delay: 0.1 }}
               viewport={{ once: true }}
               className="animate_left relative mx-auto hidden md:flex md:w-1/2 md:justify-center lg:block"
-              style={{ height: "600px", width: "500px" }} // Adjust these dimensions as needed
+              style={{ height: "670px", width: "640px" }} // Adjust these dimensions as needed
             >
               {AboutDetails.aboutimage && (
-                <div className=" h-[499px] w-[329px]">
+                <div className=" h-[670px] w-[640px]">
                   <Image
                     src={AboutDetails.aboutimage}
                     alt={AboutDetails.aboutimage}
