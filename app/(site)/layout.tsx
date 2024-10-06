7c10f42337ff76c901a4ae796ecf4b10b592dfb9
Abhinives <abhinivi67@gmail.com>
2024-10-06 19:02:15 +0530

    header changed

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -27,8 +27,18 @@ export default function RootLayout({
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="description" content="Your description here" />
         <link rel="icon" href="/images/favicon.ico" sizes="any" />
-        <link rel="icon" href="/images/favicon-32x32.png" type="image/png" sizes="32x32" />
-        <link rel="icon" href="/images/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link
+          rel="icon"
+          href="/images/favicon-32x32.png"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          href="/images/favicon-16x16.png"
+          type="image/png"
+          sizes="16x16"
+        />
       </head>
       <body className={`dark:bg-black ${inter.className}`}>
         <Provider store={store}>
@@ -38,7 +48,7 @@ export default function RootLayout({
             defaultTheme="light"
           >
             {/* <Lines /> */}
-            <Header setLanguage={setLanguage} /> {/* {{ edit_1 }} */}
+            <Header /> {/* {{ edit_1 }} */}
             <ToasterContext />
             {children}
             <Footer />
