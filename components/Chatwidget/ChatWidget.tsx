9fa33b864344c56a6bcddcc8d39c5a12ed62328d
sayvai <prakash@sayvai.io>
2024-11-05 16:31:33 +0530

    chatwidget UI changes updated

--- components/Chatwidget/ChatWidget.tsx
+++ components/Chatwidget/ChatWidget.tsx
@@ -10,22 +10,13 @@ import { BufferMemory } from "langchain/memory";
 import { SystemMessage } from "@langchain/core/messages";
 import { supabase } from "@/supabase_config/supabaseClient";
 
-// // Add memory for conversation history
-// const memory = new MemorySaver();
-// const app = workflow.compile({ checkpointer: memory });
-
 const ChatWidget = () => {
   const [customPrompt, setPrompt] = useState("");
   const [openAIKey, setOpenAIkey] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-  const [messages, setMessages] = useState<{ role: string; content: string }[]>(
-    [],
-  );
+  const [messages, setMessages] = useState<{ role: string; content: string }[]>([]);
   const [inputMessage, setInputMessage] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const [activeButton, setActiveButton] = useState<"comment" | "mic">(
-    "comment",
-  );
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
   const fetchChatBotDetails = async () => {
@@ -42,70 +33,23 @@ const ChatWidget = () => {
   useEffect(() => {
     fetchChatBotDetails();
   }, []);
-  const toggleChat = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]); // Scroll to the bottom whenever messages change
+
+  const toggleChat = () => {
+    if (isOpen) {
+      setMessages([]); // Reset messages when closing the chat
+    }
+    setIsOpen(!isOpen);
+  };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputMessage(e.target.value);
   };
-
-  // const handleSendMessage = async () => {
-  //   setLoading(true);
-  //   if (inputMessage.trim()) {
-  //     setMessages([...messages, { role: "user", content: inputMessage }]);
-  //     const currMessage = inputMessage;
-  //     setInputMessage("");
-  //     const systemMessage = {
-  //       role: "system",
-  //       content:
-  //         "You are a helpful assistant representing the organization Ecofash. If someone asks about the organization name, always respond with 'Ecofash'.",
-  //     };
-
-  //     try {
-  //       const response = await fetch(
-  //         "https://api.openai.com/v1/chat/completions",
-  //         {
-  //           method: "POST",
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //             Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-  //           },
-  //           body: JSON.stringify({
-  //             model: "gpt-3.5-turbo",
-  //             messages: [
-  //               systemMessage,
-  //               ...messages,
-  //               { role: "user", content: currMessage },
-  //             ],
-  //             temperature: 0.7,
-  //             max_tokens: 150,
-  //           }),
-  //         },
-  //       );
-
-  //       const data = await response.json();
-  //       console.log(data);
-  //       const aiMessage = data.choices[0].message.content.trim();
-  //       console.log(aiMessage);
-  //       // Add the assistant response to the chat
-  //       setLoading(false);
-  //       setMessages((prev) => [
-  //         ...prev,
-  //         { role: "assistant", content: aiMessage },
-  //       ]);
-  //     } catch (error) {
-  //       console.error("Error fetching OpenAI API:", error);
-  //       setLoading(false);
-  //       setMessages((prev) => [
-  //         ...prev,
-  //         { role: "assistant", content: "Error in getting response" },
-  //       ]);
-  //     }
-
-  //     // setTimeout(() => {
-  //     //   setMessages((prev) => [...prev, "Thanks for your message!"]);
-  //     // }, 1000);
-  //   }
-  // };
 
   const handleSendMessage = async () => {
     const model = new ChatOpenAI({
@@ -154,80 +98,75 @@ const ChatWidget = () => {
   };
 
   return (
-    <div className="fixed bottom-6 right-6">
-      <button
-        onClick={toggleChat}
-        className={`relative z-10 p-3 text-white transition-all duration-300 ${
-          isOpen ? "rounded-full bg-[#609641]" : "rounded-sm bg-[#609641]"
-        }`}
-      >
-        {isOpen ? (
-          <FaTimes className="text-xl" />
-        ) : (
+    <div className="fixed bottom-4 right-6">
+      {!isOpen && ( // Show the comment button only when the chat is closed
+        <button
+          onClick={toggleChat}
+          className={`relative z-10 p-3 text-white transition-all duration-300 rounded-sm bg-[#609641]`}
+        >
           <FaComment className="text-xl" />
-        )}
-      </button>
-      {isOpen && (
-        <div className="absolute bottom-0 right-0 mb-12 w-80 rounded-lg bg-white shadow-md">
-          <div className="relative h-80 overflow-y-auto p-4">
-            {messages.length === 0 ? (
+        </button>
+      )}
+      {isOpen && ( // Chat widget
+        <div className="bg-white shadow-lg rounded-lg p-4 w-90">
+          <div className="flex justify-between items-center mb-2 border-b pb-2">
+            <h2 className="text-lg font-bold text-[#609641] ml-2">Ecofash Chatbot</h2>
+            <button onClick={toggleChat} className="text-gray-500">
+              <FaTimes className="text-xl hover:text-[#609641]" />
+            </button>
+          </div>
+          <div className={`overflow-y-scroll h-80 hide-scrollbar custom-scrollbar`}>
+            {/* Render messages or image here */}
+            {messages.length === 0 ? ( // Check if there are no messages
               <div className="flex h-full items-center justify-center">
                 <Image
-                  src="/images/chatwidget/chat.png"
+                  src="/images/chatwidget/chatbot1.png" // Update with your image path
                   alt="Chat"
-                  width={250}
-                  height={250}
+                  width={150}
+                  height={150}
                 />
               </div>
             ) : (
-              <div>
-                {messages.map((msg, index) => (
+              messages.map((msg, index) => (
+                <div key={index} className={`flex ${msg.role === "user" ? "justify-end" : "justify-start"} mb-2`}>
+                  {msg.role === "assistant" && ( // Render the profile image for assistant messages
+                    <div className="mr-2"> {/* Margin to the right of the image */}
+                      <Image
+                        src="/images/chatwidget/chatbot.png" // Update with your profile image path
+                        alt="Chatbot"
+                        width={25} // Set the width of the image
+                        height={25} // Set the height of the image
+                        className="rounded-full" // Make the image rounded
+                      />
+                    </div>
+                  )}
                   <div
-                    key={index}
-                    className={`mb-2 flex ${
-                      msg.role == "user" ? "justify-end " : "justify-start"
+                    className={`p-2 mb-2 max-w-[80%] break-words ${
+                      msg.role === "user" 
+                        ? "bg-[#609641] text-white rounded-3xl rounded-br-sm mr-2" // Three sides rounded large, one side small
+                        : "bg-gray-200 text-black rounded-3xl rounded-tl-sm" // Adjust for assistant messages
                     }`}
                   >
-                    <span
-                      className={` rounded  px-2 py-1 ${
-                        msg.role == "user"
-                          ? "bg-[#609641] text-white"
-                          : "bg-gray-200"
-                      }`}
-                    >
-                      {msg.content}
-                    </span>
+                    <span>{msg.content}</span>
                   </div>
-                ))}
-                {isLoading && (
-                  <div className="w-10 animate-pulse rounded bg-gray-300 text-center text-gray-400">
-                    ...
-                  </div>
-                )}
-              </div>
+                </div>
+              ))
             )}
             <div ref={messagesEndRef} />
           </div>
-          <div className="border-t p-4">
-            <div className="flex h-8 w-full flex-col">
-              <div className="-mt-2 mb-2 flex w-full items-center text-sm text-gray-500">
-                <input
-                  name="msg"
-                  type="text"
-                  value={inputMessage}
-                  onChange={(e) => setInputMessage(e.target.value)}
-                  onKeyDown={handleKeyPress}
-                  placeholder="Type your message here..."
-                  className="w-full rounded-lg border border-gray-300 bg-gray-100 p-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#609641]"
-                />
-                <button onClick={handleSendMessage} className="ml-2">
-                  <PaperAirplaneIcon className="h-6 w-6 text-[#609641]" />
-                </button>
-              </div>
-              <div className="mt-2 text-right text-xs">
-                Powered by{" "}
-                <span className="font-semibold text-[#609641]">Ecofash</span>
-              </div>
+          <div className="flex mt-4 items-center">
+            <div className="relative flex-1">
+              <input
+                type="text"
+                value={inputMessage}
+                onChange={handleInputChange}
+                onKeyPress={handleKeyPress}
+                className="border rounded ml-2 p-1 w-full focus:outline-none focus:ring-1 focus:ring-[#609641]"
+                placeholder="Type a message..."
+              />
+              <button onClick={handleSendMessage} className="absolute right-0 top-0 h-full flex items-center justify-center p-2">
+                <PaperAirplaneIcon className="h-6 w-6 text-[#609641]" />
+              </button>
             </div>
           </div>
         </div>
