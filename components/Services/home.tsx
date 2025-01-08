6e84bd42ffd2e9f265df52bee22db66a7fabcb2c
sayvai <prakash@sayvai.io>
2025-01-08 13:09:41 +0530

    08012015_Issues Solved

--- components/Services/home.tsx
+++ components/Services/home.tsx
@@ -1,26 +1,26 @@
-import { FaArrowRight } from 'react-icons/fa';
+// import { FaArrowRight } from 'react-icons/fa';
 
-const ServicesHome = () => {
-  return (
-    <div className="flex flex-col md:flex-row justify-between items-center py-12 px-4 md:px-8">
-      <div className="md:w-1/2 mb-8 md:mb-0">
-        <h2 className="text-3xl font-bold mb-4">
-          Guiding Brands to a{' '}
-          <span className="text-[#609641] font-bold text-3xl">Greener</span> Future
-        </h2>
-      </div>
-      <div className="md:w-1/2">
-        <p className="mb-4">
-          Founded in 2020, Ecofash Services emerged from a deep commitment to revolutionize 
-          the fashion industry, placing sustainability and ethics at the forefront of our mission. 
-          What began as a small initiative driven by passion and a vision for change quickly evolved into
-        </p>
-        <button className="flex items-center text-black hover:underline">
-          Our Services <FaArrowRight className="ml-2" />
-        </button>
-      </div>
-    </div>
-  );
-};
+// const ServicesHome = () => {
+//   return (
+//     <div className="flex flex-col md:flex-row justify-between items-center py-12 px-4 md:px-8">
+//       <div className="md:w-1/2 mb-8 md:mb-0">
+//         <h2 className="text-3xl font-bold mb-4">
+//           Guiding Brands to a{' '}
+//           <span className="text-[#609641] font-bold text-3xl">Greener</span> Future
+//         </h2>
+//       </div>
+//       <div className="md:w-1/2">
+//         <p className="mb-4">
+//           Founded in 2020, Ecofash Services emerged from a deep commitment to revolutionize 
+//           the fashion industry, placing sustainability and ethics at the forefront of our mission. 
+//           What began as a small initiative driven by passion and a vision for change quickly evolved into
+//         </p>
+//         <button className="flex items-center text-black hover:underline">
+//           Our Services <FaArrowRight className="ml-2" />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ServicesHome;
+// export default ServicesHome;
